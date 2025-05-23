e5ffec7a3450860e515132be7716d154e51d8da8
fabiobcruz <fabinhobarbosa1997@gmail.com>
2025-05-23 17:54:34 -0300

    tarot
    nova pasta Tarot

---
+++ tarot/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/chat-serena-2/', // Substitua pelo nome do seu repositório
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
